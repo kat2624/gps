e1158ff75df351e6b84a2e3020ad00209b973c70
Andy <a9013559772@gmail.com>
2018-09-14 07:17:10 +0530

    third commit

--- app/src/main/java/com/example/andy/gpskotlin11/MapsActivity.kt
+++ app/src/main/java/com/example/andy/gpskotlin11/MapsActivity.kt
@@ -1,7 +1,23 @@
 package com.example.andy.gpskotlin11
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
+import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -9,18 +25,60 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import kotlinx.android.synthetic.main.activity_maps.*
+import kotlinx.android.synthetic.main.login_activity.*
+
+private const val PERMISSION_REQUEST = 10
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
 
+    lateinit var locationManager: LocationManager
+    private var hasGps = false
+    private var hasNetwork = false
+    private var locationGps: Location? = null
+    private var locationNetwork: Location? = null
+
+    var latti : Double? = null
+    var longi : Double? = null
+
+    var x = 1
+
+    private var permissions = arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_maps)
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        val mapFragment = supportFragmentManager
-                .findFragmentById(R.id.map) as SupportMapFragment
-        mapFragment.getMapAsync(this)
+        setContentView(R.layout.login_activity)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkPermission(permissions)) {
+                //enableView()
+                getLocation()
+            } else {
+                requestPermissions(permissions, PERMISSION_REQUEST)
+            }
+        } else {
+            //enableView()
+            getLocation()
+        }
+
+
+        //getLocation()
+
+        fullScreen()
+        loginScreen()
+
+    }
+
+
+    private fun checkPermission(permissionArray: Array<String>): Boolean {
+        var allSuccess = true
+        for (i in permissionArray.indices) {
+            if (checkCallingOrSelfPermission(permissionArray[i]) == PackageManager.PERMISSION_DENIED)
+                allSuccess = false
+        }
+        return allSuccess
     }
 
     /**
@@ -36,8 +94,174 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         mMap = googleMap
 
         // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
+
+
+        //val sydney = LatLng(-34.0, 151.0)
+        val sydney = LatLng(longi.toString().toDouble(), latti.toString().toDouble())
         mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
     }
+
+    fun login(view: View) {
+        setContentView(R.layout.activity_maps)
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager
+                .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    private fun loginScreen() {
+
+        fadeInAnimation(textView)
+        fadeInAnimation(textView2)
+        fadeInAnimation(imageView)
+        fadeInAnimation(editText)
+        fadeInAnimation(editText2)
+        fadeInAnimation(button)
+    }
+
+    private fun fadeInAnimation(textview: TextView?) {
+
+        textview?.animate()?.alpha(1f)?.duration = 2000
+    }
+
+    private fun fadeInAnimation(imageview: ImageView? = null) {
+
+        imageview?.animate()?.alpha(1f)?.duration = 2000
+    }
+
+    private fun fadeInAnimation(edittext: EditText? = null) {
+
+        edittext?.animate()?.alpha(1f)?.duration = 2000
+    }
+
+    private fun fadeInAnimation(buttonview: Button?) {
+
+        buttonview?.animate()?.alpha(1f)?.duration = 2000
+    }
+
+    private fun fullScreen() {
+
+        supportActionBar!!.hide()
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+        actionBar?.hide()
+        background.scaleType = ImageView.ScaleType.FIT_XY
+
+    }
+
+
+    @SuppressLint("MissingPermission")
+    private fun getLocation() {
+        locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        hasGps = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+        hasNetwork = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+        if (hasGps || hasNetwork) {
+
+            if (hasGps) {
+                Log.d("CodeAndroidLocation", "hasGps")
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 0F, object : LocationListener {
+                    override fun onLocationChanged(location: Location?) {
+                        if (location != null) {
+                            locationGps = location
+
+                            latti = locationGps!!.latitude
+                            longi = locationGps!!.longitude
+
+                            Log.i("Coordinates gps 1", "x= "+longi+latti)
+//                            tv_result.append("\nGPS ")
+//                            tv_result.append("\nLatitude : " + locationGps!!.latitude)
+//                            tv_result.append("\nLongitude : " + locationGps!!.longitude)
+                            Log.d("CodeAndroidLocation", " GPS Latitude : " + locationGps!!.latitude)
+                            Log.d("CodeAndroidLocation", " GPS Longitude : " + locationGps!!.longitude)
+                        }
+                    }
+
+                    override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {
+
+                    }
+
+                    override fun onProviderEnabled(provider: String?) {
+
+                    }
+
+                    override fun onProviderDisabled(provider: String?) {
+
+                    }
+
+                })
+
+                val localGpsLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+                if (localGpsLocation != null)
+                    locationGps = localGpsLocation
+            }
+            if (hasNetwork) {
+                Log.d("CodeAndroidLocation", "hasGps")
+                locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 5000, 0F, object : LocationListener {
+                    override fun onLocationChanged(location: Location?) {
+                        if (location != null) {
+                            locationNetwork = location
+
+                            latti = locationNetwork!!.latitude
+                            longi = locationNetwork!!.longitude
+
+                            Log.i("Coordinates network", "x= "+longi+latti)
+//                            tv_result.append("\nNetwork ")
+//                            tv_result.append("\nLatitude : " + locationNetwork!!.latitude)
+//                            tv_result.append("\nLongitude : " + locationNetwork!!.longitude)
+                            Log.d("CodeAndroidLocation", " Network Latitude : " + locationNetwork!!.latitude)
+                            Log.d("CodeAndroidLocation", " Network Longitude : " + locationNetwork!!.longitude)
+                        }
+                    }
+
+                    override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {
+
+                    }
+
+                    override fun onProviderEnabled(provider: String?) {
+
+                    }
+
+                    override fun onProviderDisabled(provider: String?) {
+
+                    }
+
+                })
+
+                val localNetworkLocation = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER)
+                if (localNetworkLocation != null)
+                    locationNetwork = localNetworkLocation
+            }
+
+            if (locationGps != null && locationNetwork != null) {
+                if (locationGps!!.accuracy > locationNetwork!!.accuracy) {
+
+                    latti = locationNetwork!!.latitude
+                    longi = locationNetwork!!.longitude
+
+                    Log.i("Coordinates network", "x= "+longi+latti)
+//                    tv_result.append("\nNetwork ")
+//                    tv_result.append("\nLatitude : " + locationNetwork!!.latitude)
+//                    tv_result.append("\nLongitude : " + locationNetwork!!.longitude)
+                    Log.d("CodeAndroidLocation", " Network Latitude : " + locationNetwork!!.latitude)
+                    Log.d("CodeAndroidLocation", " Network Longitude : " + locationNetwork!!.longitude)
+                } else {
+
+                    latti = locationGps!!.latitude
+                    longi = locationGps!!.longitude
+
+                    Log.i("Coordinates Gps 2", "x= "+longi+latti)
+//                    tv_result.append("\nGPS ")
+//                    tv_result.append("\nLatitude : " + locationGps!!.latitude)
+//                    tv_result.append("\nLongitude : " + locationGps!!.longitude)
+                    Log.d("CodeAndroidLocation", " GPS Latitude : " + locationGps!!.latitude)
+                    Log.d("CodeAndroidLocation", " GPS Longitude : " + locationGps!!.longitude)
+                }
+            }
+
+        } else {
+            startActivity(Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS))
+        }
+    }
+
 }
